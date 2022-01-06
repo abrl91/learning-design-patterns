2ab3484ca08a110b60d49a6f388efbc8bc9c65ff
Amit Barletz <amitbarletz@outlook.com>
2022-01-06 15:53:02 +0200

    factory method

---
+++ factory-method/factory-method.ts
@@ -0,0 +1,48 @@
+// factory methods
+interface Product {
+    operation: () => string;
+}
+
+class ConcreteProduct1 implements Product {
+    public operation(): string {
+        return '{Result of the ConcreteProduct1}';
+    }
+}
+
+class ConcreteProduct2 implements Product {
+    public operation(): string {
+        return '{Result of the ConcreteProduct2}';
+    }
+}
+
+abstract class Creator {
+    public abstract factoryMethod(): Product;
+
+    public someOperation(): string {
+        const product = this.factoryMethod();
+        return product.operation();
+    }
+}
+
+class ConcreteCreator1 extends Creator {
+    public factoryMethod(): Product {
+        return new ConcreteProduct1();
+    }
+}
+
+class ConcreteCreator2 extends Creator {
+    public factoryMethod(): Product {
+        return new ConcreteProduct2();
+    }
+}
+
+function clientCode(creator: Creator) {
+    console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
+    console.log(creator.someOperation());
+}
+
+console.log('ConcreteCreator1.');
+clientCode(new ConcreteCreator1());
+
+console.log('ConcreteCreator2.');
+clientCode(new ConcreteCreator2());
